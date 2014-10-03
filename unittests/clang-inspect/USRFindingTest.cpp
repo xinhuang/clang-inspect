9680bcd5f1af28a7eb3a7a4d38f4043e66806ce1
xinhuang <patz.hwang@gmail.com>
2014-10-03 22:34:55 +0800

    add test for inspecting variable declaration

--- unittests/clang-inspect/USRFindingTest.cpp
+++ unittests/clang-inspect/USRFindingTest.cpp
@@ -8,6 +8,13 @@
 namespace inspect {
 namespace unittest {
 
+static const char CODE[] = "\n\
+int foo(char c) {\n\
+  int a = 33;\n\
+  return a + c + 1;\n\
+}\n\
+";
+
 void testInspection(const char *code, const std::string &loc,
                     const std::string &expectName,
                     const std::string &expectType, const std::string &expectLoc) {
@@ -26,15 +33,28 @@ void testInspection(const char *code, const std::string &loc,
 }
 
 TEST(USRFindingTest, FindFunctionDecl) {
-  const char CODE[] = "\n\
-int foo(char c) {\n\
-  return c + 1;\n\
-}\n\
-";
   auto location = "2:5";
   auto expectName = "foo";
   auto expectType = "int (char)";
-  std::string expectLocation = "2:5";
+  auto expectLocation = "2:5";
+
+  testInspection(CODE, location, expectName, expectType, expectLocation);
+}
+
+TEST(USRFindingTest, FindParamDecl) {
+  auto location = "2:14";
+  auto expectName = "c";
+  auto expectType = "char";
+  auto expectLocation = "2:14";
+
+  testInspection(CODE, location, expectName, expectType, expectLocation);
+}
+
+TEST(USRFindingTest, FindVarDecl) {
+  auto location = "3:7";
+  auto expectName = "a";
+  auto expectType = "int";
+  auto expectLocation = "3:7";
 
   testInspection(CODE, location, expectName, expectType, expectLocation);
 }
