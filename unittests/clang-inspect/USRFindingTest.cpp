6aab46800c383db8d5797360d9b669a3ac4275d0
xinhuang <patz.hwang@gmail.com>
2014-10-03 22:28:35 +0800

    extract test method for USRFindingTest

--- unittests/clang-inspect/USRFindingTest.cpp
+++ unittests/clang-inspect/USRFindingTest.cpp
@@ -8,6 +8,23 @@
 namespace inspect {
 namespace unittest {
 
+void testInspection(const char *code, const std::string &loc,
+                    const std::string &expectName,
+                    const std::string &expectType, const std::string &expectLoc) {
+  USRFindingAction usrAction(loc);
+  auto factory = clang::tooling::newFrontendActionFactory(&usrAction);
+  EXPECT_TRUE(clang::tooling::runToolOnCode(factory->create(), code));
+
+  EXPECT_EQ(expectName, usrAction.getUSRSpelling());
+  EXPECT_EQ(expectType, usrAction.getUSRType());
+
+  auto locationString = usrAction.getDefinitionLocation();
+  std::string actualLocation(locationString.begin() + locationString.length() -
+                                 expectLoc.length(),
+                             locationString.end());
+  EXPECT_EQ(expectLoc, actualLocation);
+}
+
 TEST(USRFindingTest, FindFunctionDecl) {
   const char CODE[] = "\n\
 int foo(char c) {\n\
@@ -18,18 +35,8 @@ int foo(char c) {\n\
   auto expectName = "foo";
   auto expectType = "int (char)";
   std::string expectLocation = "2:5";
-  USRFindingAction usrAction(location);
-  auto factory = clang::tooling::newFrontendActionFactory(&usrAction);
-  EXPECT_TRUE(clang::tooling::runToolOnCode(factory->create(), CODE));
 
-  EXPECT_EQ(expectName, usrAction.getUSRSpelling());
-  EXPECT_EQ(expectType, usrAction.getUSRType());
-
-  auto locationString = usrAction.getDefinitionLocation();
-  std::string actualLocation(locationString.begin() + locationString.length() -
-                                 expectLocation.length(),
-                             locationString.end());
-  EXPECT_EQ(expectLocation, actualLocation);
+  testInspection(CODE, location, expectName, expectType, expectLocation);
 }
 
 }
