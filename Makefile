0c94e1b0c6827f5f0152961313f91f38e154feca
xinhuang <patz.hwang@gmail.com>
2014-10-03 22:44:50 +0800

    clean up

--- Makefile
+++ Makefile
@@ -7,26 +7,3 @@ DIRS := clang-inspect unittests
 
 include $(CLANG_LEVEL)/Makefile
 
-###
-# Handle the nested test suite.
-
-ifneq ($(PROJ_SRC_ROOT),$(PROJ_OBJ_ROOT))
-$(RecursiveTargets)::
-	$(Verb) for dir in test; do \
-	  if [ -f $(PROJ_SRC_DIR)/$${dir}/Makefile ] && [ ! -f $${dir}/Makefile ]; then \
-	    $(MKDIR) $${dir}; \
-	    $(CP) $(PROJ_SRC_DIR)/$${dir}/Makefile $${dir}/Makefile; \
-	  fi \
-	done
-endif
-
-test::
-	@ $(MAKE) -C test
-
-report::
-	@ $(MAKE) -C test report
-
-clean::
-	@ $(MAKE) -C test clean
-
-.PHONY: test report clean
--- clang-inspect/USRFinder.cpp
+++ clang-inspect/USRFinder.cpp
@@ -14,8 +14,9 @@ public:
                              const clang::SourceLocation &point)
       : sourceMgr(sourceMgr), point(point), result(nullptr) {}
 
-  bool VisitNamedDecl(const clang::NamedDecl* decl) {
-    return setResult(decl, decl->getLocation(), decl->getNameAsString().length());
+  bool VisitNamedDecl(const clang::NamedDecl *decl) {
+    return setResult(decl, decl->getLocation(),
+                     decl->getNameAsString().length());
   }
 
   const clang::NamedDecl *getNamedDecl() const { return result; }
@@ -29,7 +30,8 @@ private:
     return false;
   }
 
-  bool isPointWithin(const clang::SourceLocation& begin, const clang::SourceLocation& end) const {
+  bool isPointWithin(const clang::SourceLocation &begin,
+                     const clang::SourceLocation &end) const {
     return begin == point || end == point ||
            (sourceMgr.isBeforeInTranslationUnit(begin, point) &&
             sourceMgr.isBeforeInTranslationUnit(point, end));
@@ -40,20 +42,20 @@ namespace inspect {
 
 const clang::NamedDecl *getNamedDeclAt(clang::ASTContext &context,
                                        const clang::SourceLocation &point) {
-  const auto& sourceMgr = context.getSourceManager();
-  const auto& fileId = sourceMgr.getFileID(point);
+  const auto &sourceMgr = context.getSourceManager();
+  const auto &fileId = sourceMgr.getFileID(point);
 
   NamedDeclFindingASTVisitor visitor(sourceMgr, point);
 
   auto decls = context.getTranslationUnitDecl()->decls();
-  for(auto& decl : decls) {
-    const auto& declFileLoc = decl->getLocation();
-    const auto& declFileId = sourceMgr.getFileID(declFileLoc);
+  for (auto &decl : decls) {
+    const auto &declFileLoc = decl->getLocation();
+    const auto &declFileId = sourceMgr.getFileID(declFileLoc);
     if (declFileId != fileId) {
       continue;
     }
     visitor.TraverseDecl(decl);
-    if (const auto* result = visitor.getNamedDecl()) {
+    if (const auto *result = visitor.getNamedDecl()) {
       return result;
     }
   }
