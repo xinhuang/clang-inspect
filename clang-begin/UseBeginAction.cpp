324b50facbca20b508cf4cc484f743991eddeaf1
xinhuang <patz.hwang@gmail.com>
2014-10-07 01:07:52 +0800

    compose non-member begin/end replacement text

--- clang-begin/UseBeginAction.cpp
+++ clang-begin/UseBeginAction.cpp
@@ -1,16 +1,32 @@
 #include "UseBeginAction.h"
 
+#include "clang/Basic/SourceManager.h"
+#include "clang/Tooling/Refactoring.h"
+
 using namespace llvm;
 using namespace clang;
 using namespace clang::ast_matchers;
+
+#include <string>
 
 namespace begin {
 void UseBeginAction::run(const MatchFinder::MatchResult &result) {
   const auto *recordDecl = result.Nodes.getNodeAs<RecordDecl>("class");
   const auto *memberCall =
       result.Nodes.getNodeAs<CXXMemberCallExpr>("callExpr");
-  outs() << "Found potential non-member begin/end transformation: "
-         << recordDecl->getQualifiedNameAsString()
-         << "::" << memberCall->getMethodDecl()->getNameAsString() << "\n";
+  // FIXME: use the original class name, qualified name might has namespace
+  const auto& className = recordDecl->getQualifiedNameAsString();
+  const auto& methodName = memberCall->getMethodDecl()->getNameAsString();
+  outs() << "Found potential non-member begin/end transformation: " << className
+         << "::" << methodName << "\n";
+  const auto& sourceMgr = *result.SourceManager;
+  const auto startLoc = sourceMgr.getSpellingLoc(memberCall->getLocStart());
+  const auto endLoc = sourceMgr.getSpellingLoc(memberCall->getLocEnd());
+
+  std::string replacememnt("std::" + methodName + "(" + className + ")");
+  auto length = sourceMgr.getFileOffset(endLoc) - sourceMgr.getFileOffset(startLoc);
+  tooling::Replacement repl(sourceMgr, startLoc, length, replacememnt);
+
+  // TODO: apply replacement to rewriter.
 }
 }
