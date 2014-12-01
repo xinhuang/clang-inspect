31c602a9a534ec7a13094e36d45d6bab4e87ccec
xinhuang <xinhuang.abc@gmail.com>
2014-12-01 21:02:28 +0800

    bugfix: use instance name instead of class name

--- clang-begin/BeginMatchCallback.cpp
+++ clang-begin/BeginMatchCallback.cpp
@@ -11,14 +11,25 @@ using namespace clang::ast_matchers;
 
 namespace begin {
 
+std::string getStringInRange(const SourceManager& SM, const Stmt& ST) {
+  const auto& LocStart = ST.getLocStart();
+  const auto& LocEnd = ST.getLocEnd();
+  const char* CharStart = SM.getCharacterData(LocStart);
+  const char* CharEnd = SM.getCharacterData(LocEnd);
+  return std::string(CharStart, CharEnd + 1);
+}
+
 void BeginMatchCallback::run(const MatchFinder::MatchResult &result) {
-  const auto *recordDecl = result.Nodes.getNodeAs<RecordDecl>("class");
   const auto *memberCall =
       result.Nodes.getNodeAs<CXXMemberCallExpr>("callExpr");
-  // FIXME: use the original class name, qualified name might has namespace
-  const auto& className = recordDecl->getQualifiedNameAsString();
-  const auto& methodName = memberCall->getMethodDecl()->getNameAsString();
+
   const auto& sourceMgr = *result.SourceManager;
+  std::string refName;
+  if (const auto* memberExpr = dyn_cast<MemberExpr>(memberCall->getCallee())) {
+    refName = getStringInRange(sourceMgr, *memberExpr->getBase());
+  }
+  // FIXME: use the original class name, qualified name might has namespace
+  const auto& methodName = memberCall->getMethodDecl()->getNameAsString();
   const auto startLoc = sourceMgr.getSpellingLoc(memberCall->getLocStart());
   const auto endLoc = sourceMgr.getSpellingLoc(memberCall->getLocEnd());
 
@@ -29,7 +40,7 @@ void BeginMatchCallback::run(const MatchFinder::MatchResult &result) {
   }
 
   // FIXME: add #include <iterator>
-  std::string replacememnt("std::" + methodName + "(" + className);
+  std::string replacememnt("std::" + methodName + "(" + refName);
   auto length = sourceMgr.getFileOffset(endLoc) - sourceMgr.getFileOffset(startLoc);
   replaces.insert(tooling::Replacement(sourceMgr, startLoc, length, replacememnt));
 }
